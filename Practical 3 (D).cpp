059d2124c264a2c6aacab4aa1c39df77323174c0
meetdhameliya1108-lgtm <meetdhameliya1108@gmail.com>
2026-08-31 07:52:34 +0530

    Implicit And Explicit

---
+++ Practical 3 (D).cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+	int num1 = 10, num2 = 3;
+	float result1;
+
+	result1 = num1 / num2;
+	printf("implicit conversion = %0.2f\n", result1);
+
+	float result2;
+
+	result2 = (float)num1 / num2;
+	printf("explicit conversion = %0.2f\n", result2);
+
+	return 0;
+}
