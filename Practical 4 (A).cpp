aab12ab5c3a50d21814f90d251740d65979f1deb
meetdhameliya1108-lgtm <meetdhameliya1108@gmail.com>
2026-09-01 07:19:23 +0530

    Positive, Negative, Or Zero Using The If Statement

---
+++ Practical 4 (A).cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+ int main ()
+ 
+ {
+ 	int num;
+ 	
+ 	printf("enter a number:");
+ 	scanf("%d",&num);
+ 	
+ 	if(num>0);
+ 	
+ 	{
+ 		printf("%d is a positiv number.",num);
+	 }
+
+	 if (num<0)
+	{
+	    printf("%d is a nagative number.",num);
+	}
+	
+	if (num==0)
+	{
+		printf("the number is zero.");
+	}
+	
+	return 0;
+ }
