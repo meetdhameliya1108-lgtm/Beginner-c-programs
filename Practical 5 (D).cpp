867efdbe75f9d761c833ef00bda688628f417dbe
meetdhameliya1108-lgtm <meetdhameliya1108@gmail.com>
2026-09-18 07:58:47 +0530

    Reverse The Digits Of A Given Number

---
+++ Practical 5 (D).cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main ()
+
+{
+	int num,reverse = 0, digit;
+	
+	printf("enter a number:");
+	scanf("%d",&num);
+	
+	while(num!=0)
+	
+	{
+		digit = num%10;
+		
+		reverse = reverse*10+digit;
+		
+		num = num/10;
+		
+	}
+	
+	printf("reverse number = %d\n", reverse);
+	return 0;
+	
+}
